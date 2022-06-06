f4647b4850fbf23b5ccfc9912985faebee565600
SURAJ KUMAR MD <79073575+suraj20028@users.noreply.github.com>
2022-06-06 14:39:05 +0530

    Update binary.c

--- binary.c
+++ binary.c
@@ -3,58 +3,33 @@
 #include<time.h>
 void main()
 {
-    int a[1000],t;
+    int a[100];
     time_t st,ed;
-    int beg=0,end = 9;
-    int mid;
     int ele,flag = 0;
 
-    for(int i = 0;i<1000;i++)
+    for(int i = 0;i<100;i++)
     {
         a[i] = rand()%1000 + 1;
     }
 
-    st = time(NULL);
-    for(int p=0;p<1000;p++)
-    {
-        for(int q = 0;q<1001;q++)
-        {
-            if(a[q]>a[q+1])
-            {
-                t = a[q];
-                a[q] = a[q+1];
-                a[q+1] = t;
-            }
-        }
-    }
-    for(int k = 0;k<1000;k++)
+    for(int k = 0;k<100;k++)
     {
         printf("%d,",a[k]);
     }
 
-
-
     printf("\n");
     printf("ENTER ELEMENT TO SEARCH \n");
     scanf("%d",&ele);
+    st = time(NULL);
 
-
-    while(beg<=end)
+    for(int j = 0;j<100;j++)
     {
-        mid = (beg+end)/2;
-        if(a[mid] == ele)
+        for(int p = 0;p<10000000;p++);
+        if(a[j] == ele)
         {
             printf("\n ELEMENT FOUND");
             flag = 1;
             break;
-        }
-        if(ele > a[mid])
-        {
-            beg = mid+1;
-        }
-        if(ele < a[mid])
-        {
-            end = mid-1;
         }
     }
 
@@ -62,9 +37,8 @@ void main()
     {
         printf("\n ELEMENT NOT FOUND");
     }
-ed = time(NULL);
+    ed = time(NULL);
 
     printf("\n TIME TAKEN = %f", difftime(ed,st));
     return 0;
 }
-
