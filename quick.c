5fa63ec15ed84c6e9f8205eae7701fe34c934b5b
SURAJ KUMAR MD <79073575+suraj20028@users.noreply.github.com>
2022-07-18 15:36:11 +0530

    Update quick.c

--- quick.c
+++ quick.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 void quicksort(int arr[25],int first,int last)
 {
+
     int i, j, pivot, temp;
     if(first<last)
     {
@@ -23,29 +24,31 @@ void quicksort(int arr[25],int first,int last)
         temp=arr[pivot];
         arr[pivot]=arr[j];
         arr[j]=temp;
+        for(int p = 0; p<1000000; p++);
         quicksort(arr,first,j-1);
         quicksort(arr,j+1,last);
     }
 }
 int main()
 {
-    int i, n, arr[25];
+    int i, n;
     time_t st,ed;
     printf("ENTER ARRAY SIZE =");
     scanf("%d",&n);
-    printf("ENTER ARRAY ELEMENTS = ");
+    int arr[n];
+    printf("ENTER ARRAY ELEMENTS");
     for (int j = 0; j < n; j++)
     {
-        arr[j] = (rand() % 1000) + 1;
-        printf("%4d", arr[j]);
+        arr[j] = (rand() % 10000) + 1;
     }
     printf("\n");
     st = time(NULL);
     quicksort(arr,0,n-1);
-    ed = time();
-    printf("\n TIME TAKEN = %f \n",difftime(ed,st));
+    ed = time(NULL);
+
     printf("\nSORTED ELEMNETS = ");
     for(i=0; i<n; i++)
         printf(" %d",arr[i]);
+    printf("\n TIME TAKEN = %f \n",difftime(ed,st));
     return 0;
 }
