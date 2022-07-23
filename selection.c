50219b7174243c70bf64030162d97f190181cb34
SURAJ KUMAR MD <79073575+suraj20028@users.noreply.github.com>
2022-07-23 18:21:44 +0530

    Update selection.c

--- selection.c
+++ selection.c
@@ -1,79 +1,82 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
-
-void delay(int milli_seconds)
-{
-
-    clock_t start_time = clock();
-
-    while (clock() < start_time + milli_seconds);
-}
-
-void swap(int *xp, int *yp)
-{
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-
-void selectionSort(int arr[], int n)
-{
-    int i, j, min_idx;
-
-    for (i = 0; i < n - 1; i++)
-    {
-        delay(1);
-        min_idx = i;
-        for (j = i + 1; j < n; j++)
-            if (arr[j] < arr[min_idx])
-                min_idx =j;
-
-        swap(&arr[min_idx], &arr[i]);
-    }
-}
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i = 0; i < size; i++)
-        printf("%d\t ", arr[i]);
-    printf("\n");
-}
-
+#include <stdlib.h>
+#include<math.h>
+void selsort(int n, int a[]);
 int main()
 {
-    int c=1;
-    while(c==1)
+    int a[15000], n, i, j, ch, temp;
+    clock_t start, end;
+    while (1)
     {
-        int n = 0, choice, key;
-        clock_t start, end;
-        printf("\nEnter the size of Array : ");
-        scanf("%d", &n);
-        int arr[n];
-        for (int j = 0; j < n; j++)
-        {
-            arr[j] = (rand() % 10) + 1;
-            printf("%4d", arr[j]);
-        }
-        printf("\nMenu\n 1. Selection Sort\n 2. Exit\nEnter the choice from menu : ");
-        scanf("%d", &choice);
-        switch (choice)
+        printf("\n1:FOR MANUAL ENTRY");
+        printf("\n2:DISPLAY TIME TAKEN TO SORT ELEMENTS FROM RANGE 500 TO 15000");
+        printf("\n3:EXIT");
+        printf("\nENTER YOUR CHOICE:");
+        scanf("%d", &ch);
+        switch (ch)
         {
         case 1:
+            printf("\nENTER NUMBER OF ELEMENTS: ");
+            scanf("%d", &n);
+            printf("\nENTER ARRAY ELEMENTS: ");
+            for (i = 0; i < n; i++)
+            {
+                scanf("%d", &a[i]);
+            }
             start = clock();
-            n = sizeof(arr) / sizeof(arr[0]);
-            selectionSort(arr, n);
+            selsort(n, a);
             end = clock();
-            printf("Sorted array: \n");
-            printArray(arr, n);
-            printf("Time taken: %f ms\n", (double)(end - start) / CLK_TCK * 1000);
+            printf("\nSORTED ELEMENTS IS: ");
+            for (i = 0; i < n; i++)
+                printf("%d\t", a[i]);
+            printf("\n TIME TAKEN TO SORT %d NUMBERS IS %f SECS", n, (((double)(end - start)) / CLOCKS_PER_SEC));
             break;
         case 2:
-            c=0;
-            exit(0);
-        default:
-            printf("Invalid Choice !");
+            n = 500;
+            while (n <= 15000)
+            {
+                for (i = 0; i < n; i++)
+                {
+                    a[i]=rand()%n;
+                    //a[i] = n - i;
+                }
+                start = clock();
+                selsort(n, a);
+                for (j = 0; j < 500000; j++)
+                {
+                    temp = 38 / 600;
+                }
+                end = clock();
+                printf("\n TIME TAKEN TO SORT %d NUMBERS IS %f SECS", n, (((double)(end - start)) / CLOCKS_PER_SEC));
+                n = n + 1000;
+            }
             break;
+        case 3:
+            exit(0);
         }
+        getchar();
     }
 }
+
+void selsort(int n, int a[])
+{
+    int i, j, t, small, pos;
+    for (i = 0; i < n - 1; i++)
+    {
+        pos = i;
+        small = a[i];
+        for (j = i + 1; j < n; j++)
+        {
+            if (a[j] < small)
+            {
+                small = a[j];
+                pos = j;
+            }
+        }
+        t = a[i];
+        a[i] = a[pos];
+        a[pos] = t;
+    }
+}
+
