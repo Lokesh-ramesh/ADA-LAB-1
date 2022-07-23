95ae6281de3dcd942d01eb8ab1a4c6bd61f09660
SURAJ KUMAR MD <79073575+suraj20028@users.noreply.github.com>
2022-07-23 18:19:11 +0530

    Update merge.c

--- merge.c
+++ merge.c
@@ -1,85 +1,118 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<time.h>
-void merge(int arr[], int l, int m, int r)
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
+void split(int[],int,int);
+void combine(int[],int,int,int);
+int main()
 {
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 = r - m;
-    int L[n1], R[n2];
-    for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1+ j];
-    i = 0;
-    j = 0;
-    k = l;
-    while (i < n1 && j < n2)
+    int a[15000], n, i, j, ch, temp;
+    clock_t start, end;
+    while (1)
     {
-        if (L[i] <= R[j])
+        printf("\n1:FOR MANUAL ENTRY");
+        printf("\n2:DISPLAY TIME TAKEN TO SORT ELEMENTS FROM RANGE 500 TO 15000");
+        printf("\n3:EXIT");
+        printf("\nENTER YOUR CHOICE:");
+        scanf("%d", &ch);
+        switch (ch)
         {
-            arr[k] = L[i];
-            i++;
+        case 1:
+            printf("\nENTER NUMBER OF ARRAY ELEMENTS: ");
+            scanf("%d", &n);
+            printf("\nENTER ARRAY ELEMENTS: ");
+            for (i = 0; i < n; i++)
+            {
+                scanf("%d", &a[i]);
+            }
+            start = clock();
+            split(a, 0, n - 1);
+            end = clock();
+            printf("\nSORTED ARRAY IS: ");
+            for (i = 0; i < n; i++)
+                printf("%d\t", a[i]);
+            printf("\n TIME TAKEN TO SORT %d NUMBERS IS %f SECS", n, (((double)(end - start)) / CLOCKS_PER_SEC));
+            break;
+        case 2:
+            n = 500;
+            while (n <= 14500)
+            {
+                for (i = 0; i < n; i++)
+                {
+                     a[i]=rand()%n;
+                    //a[i] = n - i;
+                }
+                start = clock();
+                split(a, 0, n - 1);
+                for (j = 0; j < 500000; j++)
+                {
+                    temp = 38 / 600;
+                }
+                end = clock();
+                printf("\n TIME TAKEN TO SORT %d NUMBERS IS %f SECS", n, (((double)(end - start)) / CLOCKS_PER_SEC));
+                n = n + 1000;
+            }
+            break;
+        case 3:
+            exit(0);
+        }
+        getchar();
+    }
+}
+
+void split(int a[], int low, int high)
+{
+    int mid;
+    if (low < high)
+    {
+        mid = (low + high) / 2;
+        split(a, low, mid);
+        split(a, mid + 1, high);
+        combine(a, low, mid, high);
+    }
+}
+
+void combine(int a[], int low, int mid, int high)
+{
+    int c[15000], i, j, k;
+    i = k = low;
+    j = mid + 1;
+    while (i <= mid && j <= high)
+    {
+        if (a[i] < a[j])
+        {
+            c[k] = a[i];
+            ++k;
+            ++i;
         }
         else
         {
-            arr[k] = R[j];
-            j++;
+            c[k] = a[j];
+            ++k;
+            ++j;
         }
-        k++;
     }
-    while (i < n1)
+    if (i > mid)
     {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-    while (j < n2)
-    {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-}
-void mergeSort(int arr[], int l, int r)
-{
-if (l < r)
-{
-int m = l+(r-l)/2;
-for(int p=0;p<10000000;p++);
-mergeSort(arr, l, m);
-mergeSort(arr, m+1, r);
-merge(arr, l, m, r);
-}
-}
-
-void printArray(int A[], int size)
-{
-int i;
-for (i=0; i < size; i++)
-printf("%d ", A[i]);
-printf("\n");
-}
-
-int main()
-{
-int n;
-clock_t st,ed;
-    printf("ENTER SIZE OF = ");
-    scanf("%d",&n);
-    int arr[n];
-    printf("ENTER ARRAY ELEMENTS = ");
-    for (int j = 0; j < n; j++)
+        while (j <= high)
         {
-            arr[j] = (rand() % 1000) + 1;
-            printf("%4d", arr[j]);
+            c[k] = a[j];
+            ++k;
+            ++j;
         }
-     printf("\n");
-     st = clock();
-mergeSort(arr, 0, n - 1);
-ed = clock();
-printf("\n %lf",((double)(ed-st))/CLOCKS_PER_SEC);
-printf("SORTED ARRAY IS\n");
-printArray(arr, n);
-return 0;
+    }
+    if (j > high)
+    {
+        while (i <= mid)
+        {
+            c[k] = a[i];
+            ++k;
+            ++i;
+        }
+    }
+    for (i = low; i <= high; i++)
+    {
+        a[i] = c[i];
+    }
 }
+
